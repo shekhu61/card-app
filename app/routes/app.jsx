38f22031c723a2748e5dc49535d03521a480b0d1
sahil-we <sahilsb7581@gmail.com>
2026-04-05 22:49:24 +0530

    first commit

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -27,7 +27,7 @@ export default function App() {
     <ShopifyAppProvider embedded apiKey={apiKey}>
       <PolarisAppProvider i18n={en}>
         <s-app-nav>
-          <a href="/app/_index">Points Rules</a>
+          <a href="/app/rule">Points Rules</a>
        <a href="/app/rewards">Customers</a>
         
         </s-app-nav>
